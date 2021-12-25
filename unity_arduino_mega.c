9d7efbdd75c70ec0f1ab0cdf9ce9d67430189a91
GenericLibrary <sanqtsax@gmail.com>
2021-12-24 19:11:45 -0500

    port for arduino mega refactorized

--- unity_arduino_mega.c
+++ unity_arduino_mega.c
@@ -1,3 +1,4 @@
+#if defined(ARDUINO) && defined(__AVR_ATmega2560__)
 
 #include <string.h>
 #include "unity.h"
@@ -7,11 +8,16 @@ static uint32_t s_test_start, s_test_stop;
 
 void unity_putc(int c)
 {
-    if (c == '\n') {
+    if (c == '\n')
+    {
         Serial.write('\r');
         Serial.write('\n');
-    } else if (c == '\r') {
-    } else {
+    }
+    else if (c == '\r')
+    {
+    }
+    else
+    {
         Serial.write(c);
     }
 }
@@ -35,3 +41,5 @@ uint32_t unity_exec_time_get_ms(void)
 {
     return (s_test_stop - s_test_start);
 }
+
+#endif
